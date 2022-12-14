8bc99a3c44e2bcf7e3e66cd29fc461e944fc5dcf
dhiebert7251 <dan.hiebert@sbcglobal.net>
2022-12-14 13:26:35 -0600

    copied base code from previous testbed

--- FRCMecanum2023sim/src/main/java/frc/robot/Constants.java
+++ FRCMecanum2023sim/src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +14,178 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    
+  public final static class AutoConstants{
+    public static final double AUTO_SPEED = 0.5;
+  }
+
+  public final static class PowerConstants{
+    public static final int PDP = 0;
+  }
+
+  //Controller constants
+  public final static class Controllers{
+    //controller - Driver
+    public static final int DRIVER_JOYSTICK = 0;
+
+    //controller - Driver Axis maps
+    public static final int DRIVER_JOYSTICK_LEFT_X_AXIS = 0;
+    public static final int DRIVER_JOYSTICK_LEFT_Y_AXIS = 1;
+    
+    
+    /*  
+    Remove if using Modified controller - right joystick replaced with potentiometer
+    */
+    public static final int DRIVER_JOYSTICK_RIGHT_X_AXIS = 2;
+    public static final int DRIVER_JOYSTICK_RIGHT_Y_AXIS = 3;
+    
+    /*
+    Use for modified controller only
+
+    public static final int DRIVER_ROTATION = 2; //replaces right x-axis
+    */
+
+    //controller - Driver Button maps
+    public static final int DRIVER_LEFT = 1;
+    public static final int DRIVER_RIGHT = 3;
+    public static final int DRIVER_UP = 4;
+    public static final int DRIVER_DOWN = 2;
+    public static final int DRIVER_SHOULDER_TOP_LEFT = 5;
+    public static final int DRIVER_SHOULDER_TOP_RIGHT = 6;
+    public static final int DRIVER_SHOULDER_BOTTOM_LEFT = 7;
+    public static final int DRIVER_SHOULDER_BOTTOM_RIGHT = 8;
+    public static final int DRIVER_LEFT_JOYSTICK = 9;
+    public static final int DRIVER_RIGHT_JOYSTICK = 10; 
+    //TODO: add D-pad
+
+    //controller - operator
+    public static final int OPERATOR_JOYSTICK = 1;
+
+    //controller - Operator Axis maps
+    public static final int OPERATOR_JOYSTICK_LEFT_X_AXIS = 0;
+    public static final int OPERATOR_JOYSTICK_LEFT_Y_AXIS = 1;
+    public static final int OPERATOR_JOYSTICK_RIGHT_X_AXIS = 2;
+    public static final int OPERATOR_JOYSTICK_RIGHT_Y_AXIS = 3;
+
+    //controller - Driver Button maps
+    public static final int OPERATOR_LEFT = 1;
+    public static final int OPERATOR_RIGHT = 3;
+    public static final int OPERATOR_UP = 4;
+    public static final int OPERATOR_DOWN = 2;
+    public static final int OPERATOR_SHOULDER_TOP_LEFT = 5;
+    public static final int OPERATOR_SHOULDER_TOP_RIGHT = 6;
+    public static final int OPERATOR_SHOULDER_BOTTOM_LEFT = 7;
+    public static final int OPERATOR_SHOULDER_BOTTOM_RIGHT = 8;
+    public static final int OPERATOR_LEFT_JOYSTICK = 9;
+    public static final int OPERATOR_RIGHT_JOYSTICK = 10;
+    //TODO: add d-pad
+  }
+
+  public final static class Math {
+    /** The mathematical constant pi */
+    public final static double PI = 3.14159;
+    /** Multiply to get from inches to meters */
+    public final static double INCHES_2_METERS = 0.0254000;
+    /** Multiply to get from meters to inches */
+    public final static double METERS_2_INCHES = 39.3701;
+  }
+
+    /** Physical chassis measurements and dimensions */
+  public final class ChassisConstants {
+    
+    /*
+    TODO: 
+    update chassis values to actual robot measurements
+    */
+
+    /** Wheel diameter in meters */
+    public final static double WHEEL_DIAMETER = 6 * Math.INCHES_2_METERS;
+    /** Wheel circumference in meters */
+    public final static double WHEEL_CIRCUM = WHEEL_DIAMETER * Math.PI;
+    /** Width between two wheels (axle length) in meters */
+    public final static double TRACK_WIDTH = 23 * Math.INCHES_2_METERS;
+    /** Width of the robot in meters */
+    public final static double WIDTH = 27 * Math.INCHES_2_METERS;
+    /** Length of the robot in meters */
+    public final static double LENGTH = 32.3 * Math.INCHES_2_METERS; 
+
+    /* Wheel locations relative to robot center */
+
+    Translation2d LEFT_FRONT_WHEEL = new Translation2d(0.381, 0.381);
+    Translation2d RIGHT_FRONT_WHEEL = new Translation2d(0.381, -0.381);
+    Translation2d LEFT_REAR_WHEEL = new Translation2d(-0.381, 0.381);
+    Translation2d RIGHT_REAR_WHEEL = new Translation2d(-0.381, -0.381);
+    
+  }
+  
+  /** Configuration options, PID constants, and CAN Bus ID's for {@link frc.robot.subsystems.Drivetrain Drivetrain} subsystem */
+  public final static class DriveConstants {
+    /* 
+    TODO:
+    Get CANbus ID values for sparkmax controllers
+    Check drive motor INVERTED settings
+    */
+
+    //Motor IDs
+    public static final int LEFT_FRONT_MOTOR = 10;
+    public static final int LEFT_REAR_MOTOR = 11;
+    public static final int RIGHT_FRONT_MOTOR = 12;
+    public static final int RIGHT_REAR_MOTOR = 13;
+
+    //Motor inversion
+    public final static boolean LEFT_FRONT_INVERTED = false;
+    public final static boolean LEFT_REAR_INVERTED = false;
+    public final static boolean RIGHT_FRONT_INVERTED = true;
+    public final static boolean RIGHT_REAR_INVERTED = true;
+
+    //SparkMAX PID constants
+    /** P constant for SparkMAX onboard PID control */
+    public final static double P = 0.0;
+    /** I constant for SparkMAX onboard PID control */
+    public final static double I = 0.0;
+    /** D constant for SparkMAX onboard PID control */
+    public final static double D = 0.0;
+    /** F constant for SparkMAX onboard PID control */
+    public final static double F = 0.0;
+
+
+    /** Max speed of the robot between [0,1] */
+    public final static double MAX_SPEED = 0.75;
+
+    /** Maximum forward driving velocity in m/s (meters per second). Should
+     * be slightly lower than robot's maximum free speed */
+    public final static double MAX_VELOCITY = 2.0;
+
+    /** Maximum forward driving acceleration in m/s^2 (meters per second
+     * squared) */
+    public final static double MAX_ACCELERATION = 2.0;
+
+    /** Maximum rotational velocity in rad/s */
+    public final static double MAX_ROTATION = Math.PI/4.0;
+
+    /** kS constant with units V (volts) for Drivetrain motor
+     * characterization. See
+     * <a href=https://docs.wpilib.org/en/stable/docs/software/wpilib-tools/robot-characterization/introduction.html">FRC Docs</a>
+     * section on motor characterization for more */
+    public final static double S = 1.48;
+
+    /** kV constant with units (Vs)/m (volt seconds per meter) for Drivetrain
+     * motor characterization. See
+     * <a href=https://docs.wpilib.org/en/stable/docs/software/wpilib-tools/robot-characterization/introduction.html">FRC Docs</a>
+       * section on motor characterization for more */
+    public final static double V = 2.93;
+
+    /** kA constant with units (Vs^2)/m (volt seconds squared per meter) for
+     * Drivetrain motor characterization. See
+     * <a href=https://docs.wpilib.org/en/stable/docs/software/wpilib-tools/robot-characterization/introduction.html">FRC Docs</a>
+     * section on motor characterization for more */
+    public final static double A = 0.0313;
+
+    //encoders
+    public final static int kEncoderCPR = 42;
+    
+
+    }
+    
+}
--- FRCMecanum2023sim/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ FRCMecanum2023sim/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,15 +3,294 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+//REV libraries
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxRelativeEncoder.Type;
+
+//Geometry/kinematics
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.MecanumDriveOdometry;
+import edu.wpi.first.math.kinematics.MecanumDriveWheelSpeeds;
+
+//Drive
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+
+//Sensors
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+
+//Dashboard
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 public class Drivetrain extends SubsystemBase {
   /** Creates a new Drivetrain. */
-  public Drivetrain() {}
+  
+  //drivetrain speed controllers   
+  CANSparkMax leftFrontMotor;
+  CANSparkMax rightFrontMotor;
+  CANSparkMax leftRearMotor;
+  CANSparkMax rightRearMotor;
+
+  //drivetrain type
+  MecanumDrive drive;
+
+  //odometry
+  MecanumDriveOdometry driveOdometry;
+  
+  //sensors
+  Gyro gyro;
+
+  RelativeEncoder leftFrontMotorEncoder;
+  RelativeEncoder rightFrontMotorEncoder;
+  RelativeEncoder leftRearMotorEncoder;
+  RelativeEncoder rightRearMotorEncoder;
+  
+    /** Creates a new Drivetrain. */
+    public Drivetrain() {
+
+      //Motors
+      leftFrontMotor = new CANSparkMax(Constants.DriveConstants.LEFT_FRONT_MOTOR,MotorType.kBrushless);
+      leftFrontMotor.setInverted(Constants.DriveConstants.LEFT_FRONT_INVERTED);
+      leftFrontMotor.getEncoder(Type.kHallSensor,Constants.DriveConstants.kEncoderCPR);
+      
+      leftRearMotor = new CANSparkMax(Constants.DriveConstants.LEFT_REAR_MOTOR, MotorType.kBrushless);
+      leftRearMotor.setInverted(Constants.DriveConstants.LEFT_REAR_INVERTED);
+      leftRearMotor.getEncoder(Type.kHallSensor,Constants.DriveConstants.kEncoderCPR);
+      
+      rightFrontMotor = new CANSparkMax(Constants.DriveConstants.RIGHT_FRONT_MOTOR, MotorType.kBrushless);
+      rightFrontMotor.setInverted(Constants.DriveConstants.RIGHT_FRONT_INVERTED);
+      rightFrontMotor.getEncoder(Type.kHallSensor,Constants.DriveConstants.kEncoderCPR);
+     
+      rightRearMotor = new CANSparkMax(Constants.DriveConstants.RIGHT_REAR_MOTOR, MotorType.kBrushless);
+      rightRearMotor.setInverted(Constants.DriveConstants.RIGHT_REAR_INVERTED);
+      rightRearMotor.getEncoder(Type.kHallSensor,Constants.DriveConstants.kEncoderCPR);
+    
+      //mecanum drivetrain
+      drive = new MecanumDrive(leftFrontMotor, leftRearMotor, rightFrontMotor, rightRearMotor);
+      setMaxSpeed();
+      
+      //Sensors
+
+      //Gyro
+      ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+      gyro.reset();
+      Shuffleboard.getTab("Telemetry").add(gyro);
+
+      //Odometry
+      
+      //driveOdometry = new MecanumDriveOdometry(Constants.DriveConstants.KINEMATICS, getGyroRotation(), getWheelPositions());
+
+      //Drive motor encoders
+      leftFrontMotorEncoder = leftFrontMotor.getEncoder(Type.kHallSensor,Constants.DriveConstants.kEncoderCPR);
+      leftRearMotorEncoder = leftRearMotor.getEncoder(Type.kHallSensor,Constants.DriveConstants.kEncoderCPR);
+      rightFrontMotorEncoder = rightFrontMotor.getEncoder(Type.kHallSensor,Constants.DriveConstants.kEncoderCPR);
+      rightRearMotorEncoder = rightRearMotor.getEncoder(Type.kHallSensor,Constants.DriveConstants.kEncoderCPR);
+
+      /*
+      TODO: why 10.71?  where is this conversion factor from?
+       */
+      leftFrontMotorEncoder.setPositionConversionFactor(Constants.ChassisConstants.WHEEL_CIRCUM/10.71);
+      leftRearMotorEncoder.setPositionConversionFactor(Constants.ChassisConstants.WHEEL_CIRCUM/10.71);
+      rightFrontMotorEncoder.setPositionConversionFactor(Constants.ChassisConstants.WHEEL_CIRCUM/10.71);
+      rightRearMotorEncoder.setPositionConversionFactor(Constants.ChassisConstants.WHEEL_CIRCUM/10.71);
+      leftFrontMotorEncoder.setVelocityConversionFactor(Constants.ChassisConstants.WHEEL_CIRCUM/(10.71*60));
+      leftRearMotorEncoder.setVelocityConversionFactor(Constants.ChassisConstants.WHEEL_CIRCUM/(10.71*60));
+      rightFrontMotorEncoder.setVelocityConversionFactor(Constants.ChassisConstants.WHEEL_CIRCUM/(10.71*60));
+      rightRearMotorEncoder.setVelocityConversionFactor(Constants.ChassisConstants.WHEEL_CIRCUM/(10.71*60));
+ 
+    }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  // driveOdometry.update(getGyroRotation(), getWheelPositions()); 
+
+    SmartDashboard.putNumber("Front Left Position",leftFrontMotorEncoder.getPosition());
+    SmartDashboard.putNumber("Front Left Velocity",leftFrontMotorEncoder.getVelocity());
+    SmartDashboard.putNumber("Front Right Position",rightFrontMotorEncoder.getPosition());
+    SmartDashboard.putNumber("Front Right Velocity",rightFrontMotorEncoder.getVelocity());
+    SmartDashboard.putNumber("Rear Left Position",leftRearMotorEncoder.getPosition());
+    SmartDashboard.putNumber("Rear Left Velocity",leftRearMotorEncoder.getVelocity());       
+    SmartDashboard.putNumber("Rear Right Position", rightRearMotorEncoder.getPosition());
+    SmartDashboard.putNumber("Rear Right Velocity",rightRearMotorEncoder.getVelocity());  
   }
+
+  public void driveWithJoysticks(double throttle, double slide, double rotation, boolean useFOD, int joystickType) { 
+    if (useFOD) {
+        drive.driveCartesian(throttle, slide, rotation, getGyroAngle());
+      }
+      else
+      {
+        drive.driveCartesian(throttle, slide, rotation);
+      } 
+    }
+  
+    
+    /**
+    * Sets the drive motors to brake mode
+    * @see #setCoast
+    */
+    public void setBrake() {
+      leftFrontMotor.setIdleMode(IdleMode.kBrake);
+      leftRearMotor.setIdleMode(IdleMode.kBrake);
+      rightFrontMotor.setIdleMode(IdleMode.kBrake);
+      rightRearMotor.setIdleMode(IdleMode.kBrake);
+    }
+
+    /**
+     * Sets the drive motors to brake mode
+    * @see #setBrake
+    */
+    public void setCoast() {
+      leftFrontMotor.setIdleMode(IdleMode.kCoast);
+      leftRearMotor.setIdleMode(IdleMode.kCoast);
+      rightFrontMotor.setIdleMode(IdleMode.kCoast);
+      rightRearMotor.setIdleMode(IdleMode.kCoast);
+    }
+
+    /** Set the max speed of the drivetrain between [0,1] */
+    public void setMaxSpeed() {
+     drive.setMaxOutput(Constants.DriveConstants.MAX_SPEED);
+    }
+
+    
+    /** Resets the drive encoders to currently read a position of 0. */
+    public void resetEncoders() {
+      leftFrontMotorEncoder.setPosition(0);
+      leftRearMotorEncoder.setPosition(0);
+      rightFrontMotorEncoder.setPosition(0);
+      rightRearMotorEncoder.setPosition(0);
+    }
+
+
+    /** Get the distance driven by the left side */
+    public double getDistanceLeft() {
+     return (leftFrontMotorEncoder.getPosition() + leftRearMotorEncoder.getPosition())/2.0;
+    }
+
+    /** Get the distance driven by the right sice */
+    public double getDistanceRight() {
+     return (rightFrontMotorEncoder.getPosition() + rightRearMotorEncoder.getPosition())/2.0;
+    }
+  
+    /** Get the velocity of the left side */
+    public double getVelocityLeft(){
+     return (leftFrontMotorEncoder.getVelocity() + leftRearMotorEncoder.getVelocity())/2.0;
+    }
+
+    /** Get the velocity of the right side */
+    public double getVelocityRight(){
+     return (rightFrontMotorEncoder.getVelocity() + rightRearMotorEncoder.getVelocity())/2.0;
+    }  
+    
+    /**
+    * Returns the current wheel speeds of the drivetrain.
+    */
+    public MecanumDriveWheelSpeeds getWheelSpeeds() {
+      return new MecanumDriveWheelSpeeds(
+           leftFrontMotorEncoder.getVelocity(),
+           rightFrontMotorEncoder.getVelocity(),
+           leftRearMotorEncoder.getVelocity(),
+           rightRearMotorEncoder.getVelocity());
+     } 
+
+     /** Get the current robot pose as a Pose2d object in meters */
+    public Pose2d getPose() {
+      return driveOdometry.getPoseMeters();
+    }
+  
+    /** Get the current gyroscope angle in degrees from forward [-180, 180] */
+    public double getGyroAngle() {
+         return gyro.getRotation2d().getDegrees();
+    }
+    
+    /** Get the current gyroscope velocity in degrees/second,
+    * where positive is counter-clockwise */
+    public double getGyroVelocity() {
+     return -gyro.getRate();
+    }
+
+    /** Get the current gyroscope rotation as a Rotation2d object */
+    public Rotation2d getGyroRotation() {
+      return gyro.getRotation2d();
+    }
+    
+
+      /** Reset the gyroscope to 0 */
+    public void resetGyro() {
+     if (getVelocityLeft() != 0 || getVelocityRight() != 0) {
+      System.out.println("WARNING: Do not try to reset the gyroscope while the robot is moving");
+      return;
+    }
+      gyro.reset();
+    }
+
+    public void calibrateGyro(){
+      if (getVelocityLeft() != 0 || getVelocityRight() != 0) {
+        System.out.println("WARNING: Do not try to reset the gyroscope while the robot is moving");
+        return;
+      }
+      gyro.calibrate();
+    }
+
+    
+    public void stop() {
+      drive.stopMotor();
+
+    }
+
+    public void update() {
+      //potentially used to update shuffleboard sensor data
+
+    }
+    
+    /*
+     * TODO: need to see how this all works
+     */
+    private void configureDriveShuffleboard() {
+      //set up shuffleboard tab to monitor/control drive components
+      ShuffleboardTab shuffleDriveTab = Shuffleboard.getTab("Drive");
+
+      //Add motor encoder monitors
+      shuffleDriveTab
+        .add("Left Front Motor", leftFrontMotor)
+        .withPosition(0,0)
+        .withSize(2,1);
+
+      shuffleDriveTab
+        .add("Right Front Motor", rightFrontMotor)
+        .withPosition(3,0)
+        .withSize(2,1);
+
+      shuffleDriveTab
+        .add("Left Rear Motor", leftRearMotor)
+        .withPosition(0,2)
+        .withSize(2,1);
+
+      shuffleDriveTab
+        .add("Right Rear Motor", rightRearMotor)
+        .withPosition(3,2)
+        .withSize(2,1);
+
+      SmartDashboard.putString("Gyro Angle", Double.toString(gyro.getAngle()));
+
+
+      SmartDashboard.putNumber("Front Left Drive", leftFrontMotor.getOutputCurrent());
+      SmartDashboard.putNumber("Back Left Drive", leftRearMotor.getOutputCurrent());
+      SmartDashboard.putNumber("Front Right Drive", rightFrontMotor.getOutputCurrent());
+      SmartDashboard.putNumber("Back Right Drive", rightRearMotor.getOutputCurrent());
+
+      
+    }
+
 }
